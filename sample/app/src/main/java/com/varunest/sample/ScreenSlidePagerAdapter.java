1196dcc2233eb66db4fa46d2233908e6ad31113e
Varun Verma <mailvarunest@gmail.com>
2016-07-09 06:50:27 +0530

    Add Sample App Icon, Fix DemoActivity path in Manifest

--- sample/app/src/main/java/com/varunest/sample/ScreenSlidePagerAdapter.java
+++ sample/app/src/main/java/com/varunest/sample/ScreenSlidePagerAdapter.java
@@ -9,6 +9,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.varunest.sample.sparkbutton.R;
 import com.varunest.sparkbutton.SparkButton;
 
 /**
